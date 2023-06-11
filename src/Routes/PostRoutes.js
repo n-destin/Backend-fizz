69f81a838937b103ce6068f7d25658509f063a67
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2023-06-10 23:19:58 -0400

    changes

--- src/Routes/PostRoutes.js
+++ src/Routes/PostRoutes.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 const router = Router();
 import * as postControllers from '../controllers/PostControllers'
 
+router.get('/', async (req, res)=>{
+    res.json('If you recive this message, that means I hacked your account')
+})
+
 router.get('/posts', async (req, res)=>{
     try {
         const posts = await postControllers.getPosts();
@@ -23,10 +27,37 @@ router.get('/posts/:id ', async (req, res)=>{
 })
 
 // edit the post;
-
 router.put('/posts/:id', async (req, res)=>{
     const id = req.params.id;
     const Fields = req.body;
+    try {
+        const result = await postControllers.EditPost(id, Fields);
+        res.json(result);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+router.delete('/posts/:id', async (req, res)=>{
+    const id = req.params.id;
+    try {
+        const result = await postControllers.deletePost(id);
+        return res.json(result);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+// create a post
+router.post('/posts/:id', async (req, res)=>{
+    const id = req.params.id;
+    const content = req.body;
+    try {
+        const result = await postControllers.createPost(content);
+        return res.json(result);
+    } catch (error) {
+        console.log(error.message);
+    }
 })
 
 
